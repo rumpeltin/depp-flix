8af00c32cc7b6d01de52d850d58c4ca24a17bc19
rumpeltin <76797616+rumpeltin@users.noreply.github.com>
2023-02-16 16:44:30 +0000

    testing bcryptjs

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/users/:Username', passport.authenticate('jwt', {session: false}), (req
 });
 
 // GET All movies
-app.get("/movies", passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get("/movies", (req, res) => {
 	movies.find()
 		.then((movies) => {
 			res.status(201).json(movies);
